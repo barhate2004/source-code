caf23f3735640feb0e6ab29564a81c9abb947cca
barhate2004 <119530150+barhate2004@users.noreply.github.com>
2026-04-28 15:36:31 +0530

    update

--- script.js
+++ script.js
@@ -37,38 +37,3 @@ window.onload = typeWriter
 
 
 
-<section class="services">
-<div class="services-container">
-      <div class="service-box">
-          <i class="fas fa-laptop-code service-icon""></i> <h3 class="service-heading">web design</h3>
-          <p class="service-description">I design Apps that not only look beautiful but also offer exceptional functionality.</p>
-      </div>
-
-      <div class="service-box">
-        <i class="fa-brands fa-java"></i>
-        <h3 class="service-heading">Java</h3>
-        <p class="service-description">I design websites that not only look beautiful but also offer exceptional functionality.</p>
-    </div>
-    
-
-      <div class="service-box">
-          <i class="fa-brand fa-python"></i> <h3 class="service-heading">Graphics Design</h3>
-          <p class="service-description">I design graphics that not only look beautiful but also convey deep meaning and purpose.</p>
-      </div>
-      <div class="service-box">
-          <i class=" fa-solid fa-c"></i>
-          <h3 class="service-heading">Web Development</h3>
-          <p class="service-description">I develop Website that not only look beautiful but also offer exceptional functionality</p>
-      </div>
-      <div class="service-box">
-          <i class="fas fa-video service-icon"></i>
-          <h3 class="service-heading">Video Editor</h3>
-          <p class="service-description">I edit videos that not only look beautiful but also engage and captivate audiences.</p>
-      </div>
-      <div class="service-box">
-          <i class="fas fa-desktop service-icon"></i>
-          <h3 class="service-heading">UI/UX Design</h3>
-          <p class="service-description">I design UI/UX that not only looks beautiful but also understands user requirements and behavior.</p>
-      </div>
-  </div>
-</section>
